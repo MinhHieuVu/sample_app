537e1df5d9ad94d09fd63faa41e760770064d4d8
Hieu <minhhieuvux@gmail.com>
2021-09-12 16:57:56 +0700

    upadte password

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'jbuilder', '~> 2.7'
 
 gem 'bootstrap-sass', '3.4.1'
 
+gem 'bcrypt', '3.1.13'
+
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_12_022522) do
+ActiveRecord::Schema.define(version: 2021_09_12_092053) do
 
-  create_table "users", charset: "utf8mb3", collation: "utf8_unicode_ci", force: :cascade do |t|
+  create_table "users", charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
