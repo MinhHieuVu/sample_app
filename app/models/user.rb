f8b135ba45b4b48a2df7b88c08d3b7df2ed8f242
VuMinhHieu <minhhieuvux@gmail.com>
2021-12-26 13:30:57 +0700

    chapter 12

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token, :activation_token, :reset_token
   attr_accessor :remember_token, :activation_token
   before_save :downcase_email
   before_create :create_activation_digest
@@ -34,10 +35,29 @@ class User < ApplicationRecord
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
   end
-
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
   # Forgets a user.
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  # Activates an account.
+  def activate
+    update_columns(activated: FILL_IN, activated_at: FILL_IN)
+  end
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns(reset_digest: FILL_IN, reset_sent_at: FILL_IN)
+  end
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
   private
   # Converts email to all lower-case.
@@ -49,14 +69,9 @@ class User < ApplicationRecord
     self.activation_token = User.new_token
     self.activation_digest = User.digest(activation_token)
   end
-  # Activates an account.
-  def activate
-    update_columns(activated: FILL_IN, activated_at: FILL_IN)
-  end
-  # Sends activation email.
-  def send_activation_email
-    UserMailer.account_activation(self).deliver_now
-  end
+
+
+
 
 
 end
